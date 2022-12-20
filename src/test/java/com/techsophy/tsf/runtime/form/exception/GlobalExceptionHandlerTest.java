4bb5b4493e8f64bfd320a2cd5cfd0e7c13362391
Rajeev <rajeevkumar.s@techsophy.com>
2022-12-20 11:37:37 +0530

    UnitTestcase increased to 30%

---
+++ src/test/java/com/techsophy/tsf/runtime/form/exception/GlobalExceptionHandlerTest.java
@@ -0,0 +1,36 @@
+package com.techsophy.tsf.runtime.form.exception;
+
+import com.techsophy.tsf.runtime.form.config.GlobalMessageSource;
+import com.techsophy.tsf.runtime.form.model.ApiErrorResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class GlobalExceptionHandlerTest {
+
+    @Mock
+    WebRequest webRequest;
+    @InjectMocks
+    GlobalExceptionHandler globalExceptionHandler;
+    @Test
+    void recordUnableToSaveExceptionTest() {
+        RecordUnableToSaveException exception = new RecordUnableToSaveException("error", "msg");
+        ResponseEntity<ApiErrorResponse> actualResponse = globalExceptionHandler.recordUnableToSaveException(exception, webRequest);
+        ApiErrorResponse errorDetails = new ApiErrorResponse(actualResponse.getBody().getTimestamp(), exception.getMessage(), exception.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR, webRequest.getDescription(false));
+        ResponseEntity<ApiErrorResponse> expectedResponse=  new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        Assertions.assertEquals(expectedResponse, actualResponse);
+    }
+}
