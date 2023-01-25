98fe47761a5884c600493350d1831d4b3404cef7
nandini <nandini.k@techsophy.com>
2023-01-25 14:29:00 +0530

    sonarlint issues are resolving count is 47

--- src/main/java/com/techsophy/tsf/runtime/form/constants/FormModelerConstants.java
+++ src/main/java/com/techsophy/tsf/runtime/form/constants/FormModelerConstants.java
@@ -450,7 +450,9 @@ public class FormModelerConstants
     public static final String LOGGED_USER = "loggeduser";
     public static final String TOKEN = "token";
 
+
     /*MainMethodConstants*/
+
     public static final String PACKAGE_NAME = "com.techsophy.*";
     public static final String MULTI_TENANCY_PACKAGE_NAME = "com.techsophy.multitenancy.mongo.*";
     public static final String VERSION_1 = "1.0";
