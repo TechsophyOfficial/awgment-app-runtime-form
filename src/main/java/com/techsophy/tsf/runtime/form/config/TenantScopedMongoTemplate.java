0be160bd92231e4eb69318e5994c86ed6246b7bd
bhavya.a <bhavya.a@techsophy.com>
2024-06-27 17:57:33 +0530

    Mongo tenant Multitenancy with tests

--- src/main/java/com/techsophy/tsf/runtime/form/config/TenantScopedMongoTemplate.java
+++ src/main/java/com/techsophy/tsf/runtime/form/config/TenantScopedMongoTemplate.java
@@ -1,34 +1,17 @@
 package com.techsophy.tsf.runtime.form.config;
-
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.techsophy.multitenancy.mongo.config.TenantContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Component
-@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class TenantScopedMongoTemplate {
-
-  private final String tenantId;
   private final MongoTemplate mongoTemplate;
-
   @Autowired
-  public TenantScopedMongoTemplate(TenantContext tenantContext, MongoClient mongoClient) {
-    this.tenantId = tenantContext.getTenantId();
-    this.mongoTemplate = createMongoTemplate(mongoClient, this.tenantId);
+  public TenantScopedMongoTemplate(@Qualifier("mongoTemplate") MongoTemplate mongoTemplate) {
+    this.mongoTemplate = mongoTemplate;
   }
-
   public MongoTemplate getMongoTemplate() {
     return mongoTemplate;
-  }
-
-  private MongoTemplate createMongoTemplate(MongoClient mongoClient, String tenantId) {
-    // Implement connection pooling or reuse existing MongoTemplate instances here
-    MongoDatabase db = mongoClient.getDatabase(tenantId);
-    return new MongoTemplate(mongoClient, db.getName());
   }
 }
